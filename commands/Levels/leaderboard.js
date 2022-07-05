51afe959e024dca77f2172580cce6c1caac54ccd
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2022-07-05 00:36:46 -0400

    Update leaderboard.js

--- commands/Levels/leaderboard.js
+++ commands/Levels/leaderboard.js
@@ -14,7 +14,7 @@ module.exports = {
 
         const leaderboard = await Levels.computeLeaderboard(bot, rawLeaderboard, true);
 
-        const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}\n`); // We map the outputs.
+        const lb = leaderboard.map(e => `${e.position}. **${e.username}#${e.discriminator}**\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}\n`); // We map the outputs.
 
         let lbembed = new Discord.MessageEmbed()
         .setAuthor("IStay's Utilities", bot.user.avatarURL())
