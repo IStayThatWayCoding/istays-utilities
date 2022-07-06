2f8fb60a846209bbe4f2aa54ce4be55ad0f660e0
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2022-07-06 02:23:30 -0400

    Create lock.js

---
+++ commands/Moderation/lock.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: 'lock',
+    aliases: ['l'],
+    category: 'Moderation',
+    description: 'Lock command',
+    usage: `lock <reason>`,
+    run: async (bot, message, args) => {
+        if(!message.member.hasPermission("MANAGE_CHANNELS")) return;
+        if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return;
+
+        const role = message.guild.cache.get('932725047737585684');
+        let lockChannel = mesasge.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
+        if (!lockChannel) lockChannel = message.channel;
+
+        let reason = args.slice(1).join(' ');
+        if(!reason) reason = "No reason specified."
+
+        await lockChannel.updateOverwrite(role, {
+            SEND_MESSAGES: false
+        }).catch(err => console.log(err));
+        message.channel.send("Channel locked.").then(m => m.delete({ timeout: 5000 }));
+
+        let embed = new Discord.MessageEmbed()
+        .setAuthor("IStay's Utilities", bot.user.avatarURL())
+        .setTitle("Lockdown Activated")
+        .setColor("#fc0303")
+        .setDescription(`A lockdown for this channel has been issued.\n\nReason: ${reason}`);
+
+        lockChannel.send(embed)
+    }
+}
