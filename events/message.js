58fbf1f276ce560df2a081b2bbd2e16e966829f2
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2022-07-10 17:31:40 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -10,6 +10,22 @@ module.exports = async (bot, message) => {
 
     if (!message.guild) return;
 
+    if (!message.content.startsWith(prefix)) return;
+
+
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd.length === 0) return;
+
+    let command = bot.commands.get(cmd);
+    if (!command) command = bot.commands.get(bot.aliases.get(cmd));
+
+    if (command)
+        command.run(bot, message, args);
+
     if(message.channel.id === "995603783671361576"){
         
         if(!message.content.startsWith(prefix)){
@@ -173,21 +189,7 @@ module.exports = async (bot, message) => {
         }
     }
 
-    if (!message.content.startsWith(prefix)) return;
 
-
-    if (!message.member) message.member = await message.guild.fetchMember(message);
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-
-    if (cmd.length === 0) return;
-
-    let command = bot.commands.get(cmd);
-    if (!command) command = bot.commands.get(bot.aliases.get(cmd));
-
-    if (command)
-        command.run(bot, message, args);
 
    
 };
