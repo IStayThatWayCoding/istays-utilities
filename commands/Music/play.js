4da7eb10d64078d80590e6ffad97eeb6c81fe940
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2022-07-10 22:56:01 -0400

    sdf

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -7,7 +7,11 @@ module.exports = {
     run: async (bot, message, args) => {
         const music = args.join(" ")
 
-        bot.distube.play(message, music)
+        bot.distube.play(message.member.voice.channel. args.join(' ') , {
+            message,
+            textChannel: message.channel,
+            member: message.member
+        })
            
     }
 }
