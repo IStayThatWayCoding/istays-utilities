227215b9090b7f20f59263bf9d796b78aada89c1
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2022-07-11 17:01:18 -0400

    Update skip.js

--- commands/Music/skip.js
+++ commands/Music/skip.js
@@ -1,3 +1,4 @@
+var voters = []
 
 module.exports = {
     name: 'skip',
@@ -5,21 +6,6 @@ module.exports = {
     description: 'Skips the music',
     usage: `skip`,
     run: async (bot, message, args) => {
-
-        const allowedRoles = [
-            "992191733238595644",
-            "934227687306833950",
-            "963704992299122719",
-            "932721572370862110",
-            "764323362813509673",
-            "932728784942153798",
-            "932731510874837052",
-            "932731168938401843",
-            "932731763120300032",
-            "932719637873950810"
-        ]
-
-        if (!message.member.roles.cache.has('934227687306833950')) return message.channel.send("You do not have the power to skip. :/")
 
         const queue = bot.distube.getQueue(message)
 
@@ -35,10 +21,49 @@ module.exports = {
         if (!queue.autoplay && queue.songs.length <= 1){
             message.channel.send("No songs to skip!")
         }
+        
+        if (message.member.roles.cache.has('934227687306833950')){
+            voters = []
+            message.channel.send("Force skipping song...")
+            bot.distube.skip(message);
+            message.delete();
+            return;
+        }
+
+        if(voters.find(id=>id == message.author.id))
+            return;
+
+        voters.push(message.author.id)
+
+        if(voters.length != 5) {
+            message.delete()
+            message.channel.send(5-voters.length + " more vote(s) needed to skip")
+        } else if(voters.length === 5){
+            voters = []
+            message.channel.send("Skipping song")
+            bot.distube.skip(message);
+        }
+
+        // const allowedRoles = [
+        //     "992191733238595644",
+        //     "934227687306833950",
+        //     "963704992299122719",
+        //     "932721572370862110",
+        //     "764323362813509673",
+        //     "932728784942153798",
+        //     "932731510874837052",
+        //     "932731168938401843",
+        //     "932731763120300032",
+        //     "932719637873950810"
+        // ]
+
+        // if (!message.member.roles.cache.has('934227687306833950')) return message.channel.send("You do not have the power to skip. :/")
 
 
 
-        bot.distube.skip(message)
+
+
+        // bot.distube.skip(message)
 
 
     }
