e35d3efbff54b6302d57b1440d33420059beefc0
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2022-07-11 20:02:06 -0400

    Update clearqueue.js

--- commands/Music/clearqueue.js
+++ commands/Music/clearqueue.js
@@ -3,7 +3,8 @@ const colors = require('../../colors.json');
 
 module.exports = {
     name: 'clearqueue',
-    category: 'cq',
+    aliases: ['cq'],
+    category: 'Music',
     description: 'Clears the queue!',
     usage: `leave`,
     run: async (bot, message, args) => {
@@ -13,6 +14,8 @@ module.exports = {
         if(message.member.voice.channel !== message.guild.me.voice.channel) return;
 
         if (!message.member.roles.cache.has('934227687306833950')) return message.channel.send("You must have the DJ role to use this command.")
+
+        if (!queue.autoplay && queue.songs.length <= 1) return message.channel.send('Can\'t clear queue!').then(m => m.delete({ timeout: 5000 }));
         
         let queue = bot.distube.getQueue(message)
 
