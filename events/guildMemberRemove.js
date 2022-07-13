df15b7e1ee0635b53e57fa91aa5c586183b5d78a
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2022-07-13 05:57:37 -0400

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,7 +1,9 @@
 module.exports = async (bot, member, guild, args) => {
 
     const user = member.user;
-    
-    await bot.channels.cache.get('996713136465903677').setName(`🧑 Members: ${user.guild.memberCount}`)
-    await bot.channels.cache.get('996713529832919100').setName(`🥇 Goal: ${member.guild.memberCount}/50`)
+
+    var memberCount = user.guild.members.filter(member => !member.user.bot).size;
+
+    await bot.channels.cache.get('996713136465903677').setName(`🧑 Members: ${memberCount}`)
+    await bot.channels.cache.get('996713529832919100').setName(`🥇 Goal: ${memberCount}/50`)
 }
