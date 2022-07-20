0109f99b63a75174f2df3a2e1caf6a3418af4730
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2022-07-19 21:12:33 -0400

    Update leaderboard.js

--- commands/Levels/leaderboard.js
+++ commands/Levels/leaderboard.js
@@ -61,10 +61,12 @@ module.exports = {
                 }
 
 
-            response.setDescription(`:trophy: \`IStay's Resort Leaderboard\`\n\n🥇 <@${rankArr[0].id}> - **${rankArr[0].xp}** XP\n🥈 <@${rankArr[1].id}> - **${rankArr[1].xp}** XP\n🥉 <@${rankArr[2].id}> - **${rankArr[2].xp}** XP\n\`4.\` <@${rankArr[3].id}> - **${rankArr[3].xp}** XP\n\`5.\` <@${rankArr[4].id}> - **${rankArr[4].xp}** XP\n\`6.\` <@${rankArr[5].id}> - **${rankArr[5].xp}** XP\n\`7.\` <@${rankArr[6].id}> - **${rankArr[6].xp}** XP\n\`8.\` <@${rankArr[7].id}> - **${rankArr[7].xp}** XP\n\`9.\` <@${rankArr[8].id}> - **${rankArr[8].xp}** XP\n\`10.\` <@${rankArr[9].id}> - **${rankArr[9].xp}** XP`, false)
+            response.setDescription(`:trophy: \`IStay's Resort Leaderboard\`\n\n🥇 <@${rankArr[0].id}>`, false)
 
             message.channel.send(response);
 
             }
 
         }
+
+        // - **${rankArr[0].xp}** XP\n🥈 <@${rankArr[1].id}> - **${rankArr[1].xp}** XP\n🥉 <@${rankArr[2].id}> - **${rankArr[2].xp}** XP\n\`4.\` <@${rankArr[3].id}> - **${rankArr[3].xp}** XP\n\`5.\` <@${rankArr[4].id}> - **${rankArr[4].xp}** XP\n\`6.\` <@${rankArr[5].id}> - **${rankArr[5].xp}** XP\n\`7.\` <@${rankArr[6].id}> - **${rankArr[6].xp}** XP\n\`8.\` <@${rankArr[7].id}> - **${rankArr[7].xp}** XP\n\`9.\` <@${rankArr[8].id}> - **${rankArr[8].xp}** XP\n\`10.\` <@${rankArr[9].id}> - **${rankArr[9].xp}** XP
