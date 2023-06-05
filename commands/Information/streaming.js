5e780e9944b08ccdcc853975af9dde955babf4a4
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2023-06-05 01:23:08 -0400

    Update streaming.js

--- commands/Information/streaming.js
+++ commands/Information/streaming.js
@@ -13,7 +13,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
         .setTitle("Streaming Room Opened")
         .setColor(colors.red_light)
-        .setDescription('The streaming room has been opened because istay has decided to let people join on stream. In order to join, please ping istay (the owner) and he will let you in. Please remember that you will be on stream, and it is reccomennded that you have a decent mic!')
+        .setDescription('The streaming room has been opened because istay has decided to let people join on stream. In order to join, please ping istay (the owner) and he will let you in. Please remember that you will be on stream, and it is reccomennded that you have a decent mic! [VIEW THE STREAM HERE](https://twitch.tv/istaythatway)')
 
         message.channel.send(embed)
         message.channel.send('@everyone').then(m => m.delete({ timeout: 5000 }))
